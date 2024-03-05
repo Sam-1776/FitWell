ca7c7b2f1deeded3922db14182c8af0d4e92cfba
Samuele Simeone <69748013+Sam-1776@users.noreply.github.com>
2024-03-05 10:52:03 +0100

    First Commit

---
+++ BackEnd-FitWell/src/main/java/sameuelesimeone/FitWell/FitWellApplication.java
@@ -0,0 +1,13 @@
+package sameuelesimeone.FitWell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitWellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitWellApplication.class, args);
+	}
+
+}
---
+++ BackEnd-FitWell/src/test/java/sameuelesimeone/FitWell/FitWellApplicationTests.java
@@ -0,0 +1,13 @@
+package sameuelesimeone.FitWell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitWellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
