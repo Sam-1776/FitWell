381a8a2d7f395e556155610b550e92ac2c28a12c
Samuele Simeone <69748013+Sam-1776@users.noreply.github.com>
2024-03-26 11:59:37 +0100

    add cross origin

--- BackEnd-FitWell/src/main/java/sameuelesimeone/FitWell/controllers/NoteBookController.java
+++ BackEnd-FitWell/src/main/java/sameuelesimeone/FitWell/controllers/NoteBookController.java
@@ -3,10 +3,7 @@ package sameuelesimeone.FitWell.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sameuelesimeone.FitWell.dto.StatDDTO;
 import sameuelesimeone.FitWell.dto.StatWDTO;
 import sameuelesimeone.FitWell.models.NoteBook.NoteBookD;
@@ -19,6 +16,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/noteBook")
+@CrossOrigin(origins = "http://localhost:4200")
 public class NoteBookController {
 
     @Autowired
@@ -29,13 +27,13 @@ public class NoteBookController {
 
     @PatchMapping("/gym/{noteBookWId}")
     @PreAuthorize("hasAnyAuthority('USER')")
-    public NoteBookW updateStat(@PathVariable UUID noteBookWId, @AuthenticationPrincipal User user, StatWDTO statWDTO){
+    public NoteBookW updateStat(@PathVariable UUID noteBookWId, @AuthenticationPrincipal User user, @RequestBody StatWDTO statWDTO){
         return noteBookWService.modNote(noteBookWId, user, statWDTO);
     }
 
     @PatchMapping("/diet/{noteBookDId}")
     @PreAuthorize("hasAnyAuthority('USER')")
-    public NoteBookD updateStat(@PathVariable UUID noteBookDId, @AuthenticationPrincipal User user, StatDDTO statDDTO){
+    public NoteBookD updateStat(@PathVariable UUID noteBookDId, @AuthenticationPrincipal User user, @RequestBody StatDDTO statDDTO){
         return noteBookDService.modNote(noteBookDId, user, statDDTO);
     }
 
